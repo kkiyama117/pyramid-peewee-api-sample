5eb53174c39c49e5e0bcc181ab0229d4aa938c0c
kkiyama117 <k.kiyama117@gmail.com>
2018-10-24 15:56:33 +0900

    Fix and remove default settings

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -19,23 +17,23 @@ requires = [
 
 tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
-    'pytest',  # includes virtualenv
+    'pytest',
     'pytest-cov',
 ]
 
 setup(name='backhp',
-      version='0.0',
-      description='backhp',
-      long_description=README + '\n\n' + CHANGES,
+      version='0.0.1',
+      description='KUJinaja-sub back API server',
+      long_description=README,
       classifiers=[
           "Programming Language :: Python",
           "Framework :: Pyramid",
           "Topic :: Internet :: WWW/HTTP",
           "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
       ],
-      author='',
-      author_email='',
-      url='',
+      author='muskuarede',
+      author_email='k.kiyama117@gmail.com',
+      url='https://ku-jinja.net',
       keywords='web pyramid pylons',
       packages=find_packages(),
       include_package_data=True,

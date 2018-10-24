1b1427a417d42bb355a2a8b578aaa8bbd64dbfe9
kkiyama117 <k.kiyama117@gmail.com>
2018-10-24 21:17:22 +0900

    Fix

--- backhp/views.py
+++ backhp/views.py
@@ -1,12 +1,22 @@
+import json
+
 from pyramid.view import view_config
 from pyramid.response import Response
 
 
-@view_config(route_name='home', renderer='json',
-             request_method='GET')
+@view_config(route_name='home')
 def home(request):
     # https://docs.pylonsproject.org/projects/pyramid/en/latest/api/request.html
+    # Response
     response = request.response
-    result = {'project': 'BackHP'}
-    response.body = result
+    response.content_type = 'application/json'
+    response.status_code = 200
+    # body
+    set_json_to_responce(response, {'project': 'BackHP'})
+    return response
+
+
+def set_json_to_responce(response, json_data):
+    response = response
+    response.text = json.dumps(json_data)
     return response
